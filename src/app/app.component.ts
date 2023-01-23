ad1cbf77bab7913703c44b9484a57d9e982d09c9
“Surajit <surajitkaruri@gmail.com>
2023-01-24 01:56:14 +0530

    grid structure for headline stories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface NewsCard{
+  headline: string,
+  img?: string,
+  publishedOn: Date;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'news-station';
+  headlineStories!: NewsCard[];
+
+  ngOnInit(){
+    this.headlineStories = [
+      {
+        headline: 'News Heading 1',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 2',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 3',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 4',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 5',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 6',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 7',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 8',
+        publishedOn: new Date()
+      },
+      {
+        headline: 'News Heading 9',
+        publishedOn: new Date()
+      }
+    ]
+  }
 }
